5e1baaa9e1a08d0b52c0d2550e7ff61b56fabc5a
resteve <resteve@zikzakmedia.com>
2015-12-01 11:26:06 +0100

    Tests

--- tests/test_stock_product_location_sequence.py
+++ tests/test_stock_product_location_sequence.py
@@ -1,25 +1,14 @@
-#!/usr/bin/env python
 # This file is part of the stock_product_location_sequence module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-from trytond.tests.test_tryton import test_view, test_depends
-import trytond.tests.test_tryton
 import unittest
+import trytond.tests.test_tryton
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class StockProductLocationSequenceTestCase(unittest.TestCase):
+class StockProductLocationSequenceTestCase(ModuleTestCase):
     'Test Stock Product Location Sequence module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('stock_product_location_sequence')
-
-    def test0005views(self):
-        'Test views'
-        test_view('stock_product_location_sequence')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'stock_product_location_sequence'
 
 
 def suite():
